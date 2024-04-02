8631abfe6251e5fb0da9c9a9fee5ca4ab25fe50f
Amraj Koonar <amrajk31@gmail.com>
2024-04-01 19:29:05 -0700

    Updated main.py and twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,25 +1,18 @@
 import tweepy
 from keys_test import api_key, api_secret, bearer_token, access_token, access_token_secret
+from main import get_weather_info, get_weather_update
+import time
 
 client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
-client.create_tweet(text = "Hello world")
 
-# client.like("1613078224539615233")
+while(True):
+    weatherInfo = get_weather_info()  #returns the a sentence describing the weather 
+    weather = get_weather_update()    #returns the weather (dry, snow, rain etc)
 
-# client.retweet("1613078224539615233")
-
-# client.create_tweet(in_reply_to_tweet_id="1613078224539615233", text = "Keep learning Simplilearners")
-
-# for tweet in api.home_timeline():
-#     print(tweet.text)
-
-# person = client.get_user(username = "narendramodi").data.id
-
-# for tweet in client.get_users_tweets(person).data:
-#     print(tweet.text)
+    client.create_tweet(text = "Hello world")
 
 
 
